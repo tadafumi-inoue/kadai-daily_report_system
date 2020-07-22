b1d78ef76eaf7ab58b4d6276cd5896a765fc9cf8
tadafumi.inoue <t.inoue0319@gmail.com>
2020-07-22 15:44:43 +0900

    Add Report new

--- src/controllers/reports/ReportsIndexServlet.java
+++ src/controllers/reports/ReportsIndexServlet.java
@@ -18,13 +18,13 @@ import utils.DBUtil;
  * Servlet implementation class IndexServlet
  */
 @WebServlet("/reports/index")
-public class IndexServlet extends HttpServlet {
+public class ReportsIndexServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public ReportsIndexServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
